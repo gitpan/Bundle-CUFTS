b81fcf830a844ab105aa410e1939fb7d9b92261b
Todd Holbrook <holbrook@cpan.org>
2005-11-21 23:08:21 +0000

    Import of HOLBROOK/Bundle-CUFTS-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-CUFTS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         HOLBROOK/Bundle-CUFTS-0.01.tgz
    gitpan-cpan-author:       HOLBROOK
    gitpan-cpan-maturity:     released

---
+++ CUFTS.pm
@@ -0,0 +1,72 @@
+package Bundle::CUFTS;
+
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Bundle::CUFTS - All Perl modules required to run CUFTS.
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::CUFTS'>
+
+=head1 DESCRIPTION
+
+Installs everything you need to run CUFTS.
+
+=head1 CONTENTS
+
+Bundle::Catalyst
+
+Business::ISSN
+
+Class::Accessor
+
+Class::DBI
+
+Class::DBI::AbstractSearch
+
+Class::DBI::Plugin::CountSearch
+
+Date::Calc
+
+Exception::Class
+
+Exception::Class::DBI
+
+Getopt::Long
+
+HTML::Entities
+
+LWP::UserAgent
+
+MARC::Record
+
+MIME::Lite
+
+Net::SMTP
+
+SQL::Abstract
+
+Template
+
+Term::Readline
+
+Text::CSV
+
+URI::Escape
+
+=head1 AUTHOR
+
+Todd Holbrook, C<tholbroo@sfu.ca>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
